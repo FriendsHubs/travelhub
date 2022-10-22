10a7e7ea2691f4da85bc2597db36e7a17ebe877c
Joel Oguntoye <joeloguntoye@gmail.com>
2022-10-22 22:45:27 +0100

    added tag to ec2 instance for app frontend

--- Terraform/main.tf
+++ Terraform/main.tf
@@ -7,26 +7,26 @@ resource "aws_security_group" "frontend_sg" {
     cidr_blocks = [
       "0.0.0.0/0"
     ]
-from_port = 22
-    to_port = 22
-    protocol = "tcp"
+    from_port = 22
+    to_port   = 22
+    protocol  = "tcp"
   }
-// Terraform removes the default rule
+  // Terraform removes the default rule
   egress {
-   from_port = 0
-   to_port = 0
-   protocol = "-1"
-   cidr_blocks = ["0.0.0.0/0"]
- }
-  
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
 }
 resource "aws_instance" "public_frontend" {
-  ami           = "ami-0493936afbe820b28"
-  instance_type = "t2.micro"
-  key_name      = "jenkins-key"
+  ami                    = "ami-0493936afbe820b28"
+  instance_type          = "t2.micro"
+  key_name               = "jenkins-key"
   vpc_security_group_ids = ["${aws_security_group.frontend_sg.id}"]
 
-  # tags = {
-  #   "Name" = "travelhub_frontend"
-  # }
+  tags = {
+    "Name" = "travelhub_frontend"
+  }
 }
