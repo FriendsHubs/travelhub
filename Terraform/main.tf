b6605ebe6eec73d285cae72e7be8e6231f47c16a
Joel Oguntoye <joeloguntoye@gmail.com>
2022-10-27 10:47:34 +0100

    Removed EC2 instance declaration from Terraform

--- Terraform/main.tf
+++ Terraform/main.tf
@@ -12,36 +12,36 @@ provider "aws" {
 }
 
 
-resource "aws_security_group" "frontend_sg" {
-  ingress {
-    cidr_blocks = [
-      "0.0.0.0/0"
-    ]
-    from_port = 22
-    to_port   = 22
-    protocol  = "tcp"
-  }
-  // Terraform removes the default egress rule, so we need to add it
-  egress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
+# resource "aws_security_group" "frontend_sg" {
+#   ingress {
+#     cidr_blocks = [
+#       "0.0.0.0/0"
+#     ]
+#     from_port = 22
+#     to_port   = 22
+#     protocol  = "tcp"
+#   }
+#   // Terraform removes the default egress rule, so we need to add it
+#   egress {
+#     from_port   = 0
+#     to_port     = 0
+#     protocol    = "-1"
+#     cidr_blocks = ["0.0.0.0/0"]
+#   }
 
-}
-
-resource "aws_instance" "public_frontend" {
-  ami                    = "ami-0493936afbe820b28"
-  instance_type          = "t2.micro"
-  key_name               = "jenkins-key"
-  vpc_security_group_ids = ["${aws_security_group.frontend_sg.id}"]
-
-  tags = {
-    "Name" = "travelhub_frontend"
-  }
-}
-
-# output "frontend_ip" {
-#   value = "aws_instance.public_frontend.public_ip"
 # }
+
+# resource "aws_instance" "public_frontend" {
+#   ami                    = "ami-0493936afbe820b28"
+#   instance_type          = "t2.micro"
+#   key_name               = "jenkins-key"
+#   vpc_security_group_ids = ["${aws_security_group.frontend_sg.id}"]
+
+#   tags = {
+#     "Name" = "travelhub_frontend"
+#   }
+# }
+
+# # output "frontend_ip" {
+# #   value = "aws_instance.public_frontend.public_ip"
+# # }
