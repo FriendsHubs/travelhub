f19a9f23b57c09e9ca935d63b2cb74c711519a99
ekamanelly <ekamanelly@gmail.com>
2022-11-17 21:26:27 +0100

    added backend

--- Terraform/backend.tf
+++ Terraform/backend.tf
@@ -1,9 +1,9 @@
-# terraform {
-#   backend "s3" {
-#     bucket         = "friendshub-terraform-backend"
-#     key            = "friendshub/ekamastate"
-#     region         = "eu-west-3"
-#     dynamodb_table = "friendshub-state-lock-table"
+terraform {
+  backend "s3" {
+    bucket         = "friendshub-terraform-backend"
+    key            = "friendshub/statefile"
+    region         = "eu-west-3"
+    dynamodb_table = "friendshub-state-lock-table"
 
-#   }
-# }
+  }
+}
